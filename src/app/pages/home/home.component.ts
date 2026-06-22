8a87686baf5ff70f6de5d1532a46d2a9e68c5cd2
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-06-22 18:20:55 +0000

    ajustados

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { MenuBarComponent } from '@components/menu-bar/menu-bar.component';
+import { CardComponent } from "@app/components/card/card.component";
 
 @Component({
   selector: 'app-home-component',
   standalone: true,
   imports: [
-    MenuBarComponent
-  ],
+    MenuBarComponent,
+    CardComponent
+],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
